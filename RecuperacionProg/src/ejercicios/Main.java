de5ce8353bd62ea810274e8393670b2db9024484
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2023-06-06 20:23:34 +0200

    Update Main.java

--- RecuperacionProg/src/ejercicios/Main.java
+++ RecuperacionProg/src/ejercicios/Main.java
@@ -17,8 +17,8 @@ public class Main {
 		Scanner all = new Scanner(System.in);
 
 		do {
-			System.out.println("1. Añadir producto" + "\n" + "2.Listar productos" + "\n" + "3.Eliminar producto" + "\n" + "4.Modificar producto" +"\n"
-					+ "0.Salir");
+			System.out.println("1. Añadir producto" + "\n" + "2.Listar productos" + "\n" + "3.Eliminar producto" + "\n"
+					+ "4.Modificar producto" + "\n" + "0.Salir");
 			opcion = all.nextInt();
 			all.nextLine();
 			switch (opcion) {
@@ -30,14 +30,15 @@ public class Main {
 				if (productos.contains(p)) { // Si productos contiene un producto del mismo nombre, se cancela la opción
 												// haciéndoselo saber al usuario.
 					System.out.println("El producto ya existe");
-					
+
 				} else {
 					System.out.println("Introduzca el precio del producto");
 					precioProducto = all.nextDouble();
 					all.nextLine();
 					do {
-						System.out.println("¿El producto es perecedero o noPerecedero?"); 
-						System.out.println("Por favor, sólo introduzca perecedero o noPerecedero");													
+						System.out.println("¿El producto es perecedero o noPerecedero?"); // Si no metes nada, se añade
+						System.out.println("Por favor, sólo introduzca perecedero o noPerecedero"); // Condición
+						// vacío extremo
 						tipoProducto = all.next();
 						all.nextLine();
 
@@ -66,7 +67,7 @@ public class Main {
 					}
 					productos.add(p);
 					System.out.println("El producto se añadió");
-					
+
 				}
 				break;
 			case 2: // Esto simplemente imprime la lista de los productos.
@@ -91,76 +92,64 @@ public class Main {
 					System.out.println("El producto no existe");
 				}
 				break;
-				
+
 			case 4:
 				System.out.println("Introduzca el nombre del producto a modificar");
-				nombreProducto=all.next();
-				int opcion2=0; //Importante tener esto para después preguntar qué se quiere modificar
+				nombreProducto = all.next();
+				int opcion2 = 0; // Importante tener esto para después preguntar qué se quiere modificar
 				all.nextLine();
-				p=new Producto(nombreProducto);
-				if (productos.contains(p)) { //?
+				p = new Producto(nombreProducto);
+				if (productos.contains(p)) { // ?
 					System.out.println("¿Qué dato del producto desea modificar?");
-					
-					//TODO Si el producto no es perecedero, imprimir un menu, y si lo es, imprimir otro menu (piden otro tipo de datos)
-					
-					System.out.println("1. El precio del producto"); //Esto se va a imprimir sí o sí en ambos tipos del producto
-					for (Producto product:productos) {
-						if (product.equals(p)){ //Comprueba si algún producto es equivalente al nombre que se introdujo
-	
-							if (product instanceof NoPerecedero) { //Si NO es perecedero
-	
+
+					// TODO Si el producto no es perecedero, imprimir un menu, y si lo es, imprimir
+					// otro menu (piden otro tipo de datos)
+
+					System.out.println("1. El precio del producto"); // Esto se va a imprimir sí o sí en ambos tipos del
+																		// producto
+					for (Producto product : productos) {
+						if (product.equals(p)) { // Comprueba si algún producto es equivalente al nombre que se
+													// introdujo
+
+							if (product instanceof NoPerecedero) { // Si NO es perecedero
+
 								System.out.println("2.El tipo");
-								}
-								else if(product instanceof Perecedero) {
+							} else if (product instanceof Perecedero) {
 								System.out.println("2. Los días de caducación");
+							}
+							// Acaba el menú aquí
+
+							// Comprobación de la opción elegida
+							do {
+								if (opcion2 != 0) { // Para que en la primera vez no imprima opción no válida sin que el
+													// usuario no haya puesto nada.
+									System.out.println("opción no válida");
 								}
-								//Acaba el menú aquí
-								
-								//Comprobación de la opción elegida
-								do {
-								if (opcion2!=0) { //Para que en la primera vez no imprima opción no válida sin que el usuario no haya puesto nada.
-								System.out.println("opción no válida");
-								}
-								opcion2=all.nextInt();
+								opcion2 = all.nextInt();
 								all.nextLine();
-								}while(opcion2!=1 && opcion2!=2);
-							
-							if (opcion2==1) { //Modificar el precio del producto
+							} while (opcion2 != 1 && opcion2 != 2);
+
+							if (opcion2 == 1) { // Modificar el precio del producto
 								System.out.println("Introduzca el precio");
 								product.setPrecio(all.nextDouble());
 								System.out.println("producto modificado");
-							}
-							else {
+							} else {
 								if (product instanceof Perecedero) {
 									System.out.println("Introduzca los días a caducar");
-									Perecedero per=(Perecedero)product;
+									Perecedero per = (Perecedero) product;
 									per.setDíasCaducar(all.nextInt());
 									System.out.println("producto modificado");
-								}
-								else if(product instanceof NoPerecedero) {
+								} else if (product instanceof NoPerecedero) {
 									System.out.println("Introduzca el tipo");
-									NoPerecedero noper = (NoPerecedero)product;
+									NoPerecedero noper = (NoPerecedero) product;
 									noper.setTipo(all.next());
 									System.out.println("producto modificado");
 								}
 							}
-								
-							}
-//							else if(product instanceof Perecedero) { //Si es perecedero
-//							System.out.println("2. Días a caducar del producto");	
-//							if (opcion2!=0) { //Para que en la primera vez no imprima opción no válida sin que el usuario no haya puesto nada.
-//							System.out.println("opción no válida");
-//							}
-//							opcion2=all.nextInt();
-//							all.nextLine();
-//							if (opcion2==1) { //Modificar el precio del producto
-//								p.precio=all.nextDouble();
-//							}
-//							opcion2=0; //Para que al repetir esta opción no imprima "opción no válida" directamente, ya que chequea de que opcion2 sea 0.
-//							}
-						}			
+
+						}
 					}
-				else {
+				} else {
 					System.out.println("El producto no existe");
 				}
 				break;
@@ -171,6 +160,7 @@ public class Main {
 				System.out.println("Opción no válida");
 			}
 
-		}while(opcion!=0);all.close();
+		} while (opcion != 0);
+		all.close();
 	}
 }

bd5d8882af72a51d5cf2d54d34ce9026c336cb46
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2023-06-15 14:40:59 +0200

    Update Electrodomestico.java

--- RecuperacionProg/src/ejercicios2/Electrodomestico.java
+++ RecuperacionProg/src/ejercicios2/Electrodomestico.java
@@ -2,174 +2,200 @@ package ejercicios2;
 
 public class Electrodomestico implements Comparable<Electrodomestico> {
 
+	/**
+	 *  Color es un enumerado, su atributo se llama colores y por defecto es blanco
+	 */
+	protected enum Color{blanco, negro, rojo, azul, gris};
+
+	/**
+	 * Consumo es igual que colores, se llama consumacion.
+	 */  
+	protected enum Consumo {A,B,C,D,E,F};
+	
+	/**
+	 * Codigo es un string y la clave primaria
+	 */
 	protected String codigo = "";
+	
+	/**
+	 * Precio es el precio del electro y un double
+	 */
 	protected double precioBase = 100;
-	protected String color = "blanco";
-	protected char consumo = 'F';
+	/**
+	 * Por ultimo está el peso, que es un int.
+	 */
 	protected int peso = 5;
-
+	
 	/**
-	 * @param codigo     del electrodomestico, es un int
-	 * @param precioBase del electrodomestico, double
-	 * @param color      del electrodomestico, array de Strings
-	 * @param consumo    del electrodomestico, char que tendría que ser de la A a la F; por defecto es F
-	 * @param peso       del electrodomestico, un int.
+	 * El tipo Color que por defecto será blanco
 	 */
-	public Electrodomestico(String codigo, double precioBase, String color, char consumo, int peso) {
-		this.codigo = codigo;
-		this.precioBase = precioBase;
-		this.color = color;
-		this.consumo = consumo;
-		this.peso = peso;
-	}
-
+	protected Color color=Color.blanco;
+	
 	/**
-	 * @param codigo del electrodomestico
+	 * El tipo consumo que por defecto será F
 	 */
-	public Electrodomestico(String codigo) {
-		this.codigo = codigo;
-	}
+	protected Consumo consumacion=Consumo.F;
 
+	
+	
 	/**
-	 * 
-	 * @param codigo     del electrodomestico
-	 * @param precioBase del electrodomestico
-	 * @param peso       del electrodomestico
+	 * Constructor con todos los campos
+	 * @param codigo
+	 * @param precioBase
+	 * @param peso
+	 * @param colores
+	 * @param consumacion
 	 */
-	public Electrodomestico(String codigo, double precioBase, int peso) {
+	protected Electrodomestico(String codigo, double precioBase, int peso, String color, char consumo) {
+		super();
+		comprobarColor(color);
+		comprobarConsumo(consumo);
 		this.codigo = codigo;
 		this.precioBase = precioBase;
 		this.peso = peso;
+
 	}
 
 	/**
-	 * Constructor por defecto sin parámetros.
+	 * Constructor por defecto
 	 */
-	public Electrodomestico() {
-
+	protected Electrodomestico() {
+		super();
+	}
+	
+	/**
+	 * Constructor solo codigo
+	 * @param codigo
+	 */
+	protected Electrodomestico(String codigo) {
+		super();
+		this.codigo = codigo;
 	}
 
 	/**
-	 * Get del código
+	 * Constructor con los campos codigo, precio y el peso
+	 * @param codigo
+	 * @param precioBase
+	 * @param peso
+	 */
+	protected Electrodomestico(String codigo, double precioBase, int peso) {
+		super();
+		this.codigo = codigo;
+		this.precioBase = precioBase;
+		this.peso = peso;
+	}
+
+
+	/**
 	 * 
-	 * @return código del electrodomestico
+	 * @return codigo del electro
 	 */
 	public String getCodigo() {
 		return codigo;
 	}
 
 	/**
-	 * set del código
-	 * 
-	 * @param codigo del electrodomestico
+	 * Set del codigo
+	 * @param codigo
 	 */
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
 
 	/**
-	 * get del precioBase
-	 * 
-	 * @return el precio de base del electrodomestico
+	 * Get del precio del elctrodomestico
+	 * @return el precio del electro
 	 */
 	public double getPrecioBase() {
 		return precioBase;
 	}
 
 	/**
-	 * set del precio base del electrodomestico
-	 * 
-	 * @param precioBase del electrodomestico
+	 * Set del precio del electro
+	 * @param precioBase
 	 */
 	public void setPrecioBase(double precioBase) {
 		this.precioBase = precioBase;
 	}
 
 	/**
-	 * get color del electrodomestico
-	 * 
-	 * @return el color del electrodomestico
-	 */
-	public String getColor() {
-		return color;
-	}
-
-	/**
-	 * set del color del electrodomestico
-	 * 
-	 * @param color del electrodomestico
-	 */
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	/**
-	 * get del consumo del electrodomestico
-	 * 
-	 * @return consumo del electrodomestico
-	 */
-	public char getConsumo() {
-		return consumo;
-	}
-
-	/**
-	 * setConsumo del electrodomestico
-	 * 
-	 * @param consumo del electrodomestico
-	 */
-	public void setConsumo(char consumo) {
-		this.consumo = consumo;
-	}
-
-	/**
-	 * get del peso del electrodomestico
-	 * 
-	 * @return el peso del electrodomestico
+	 * get del peso del electro
+	 * @return peso
 	 */
 	public int getPeso() {
 		return peso;
 	}
 
 	/**
-	 * set peso del electrodomestico
-	 * 
-	 * @param peso del electrodomestico
+	 * Set del peso del electro
+	 * @param peso
 	 */
 	public void setPeso(int peso) {
 		this.peso = peso;
 	}
 
-	private static char comprobarConsumoEnergetico(char letra) {
-		if (letra != 'A' && letra != 'B' && letra != 'C' && letra != 'D' && letra != 'E' && letra != 'F') {
-			return 'F';
-		} else {
-			return letra;
-		}
+	/**
+	 * Get de los colores del electro
+	 * @return los colores
+	 */
+	public String getColores() {
+		return String.valueOf(color);
 	}
 
-	private static String comprobarColor(String color) {
-		if (!color.equals("blanco") || !color.equals("negro") || !color.equals("rojo") || !color.equals("azul")
-				|| !color.equals("gris")) {
-			return "blanco";
-		} else {
-			return color;
-		}
+	/**
+	 * Set de los colores
+	 * @param colores
+	 */
+	public void setColores(String colores) {
+		comprobarColor(colores);
 	}
+
+	/**
+	 * Get del enumerado del consumo
+	 * @return consumacion
+	 */
+	public char getConsumacion() {
+		return String.valueOf(consumacion).charAt(0);
+	}
+
+	/**
+	 * Set de la consumacion
+	 * @param consumacion
+	 */
+	public void setConsumacion(char consumacion) {
+		comprobarConsumo(consumacion);
+	}
+
+	/**
+	 * Compare to del electrodomestico
+	 */
+	@Override
+	public int compareTo(Electrodomestico el) {
+		return this.codigo.compareTo(el.codigo);
+	}
+
+	/**
+	 * Precio final que tendrá una serie de cálculos; dependiendo del peso  del electro costará más o menos
+	 */
 
 	public void precioFinal() {
-		switch (consumo) {
-		case 'A':
+		switch (consumacion.toString()) {
+		case "A":
 			precioBase += 100;
 			break;
-		case 'B':
+		case "B":
 			precioBase += 80;
-		case 'C':
+			break;
+		case "C":
 			precioBase += 60;
-		case 'D':
+			break;
+		case "D":
 			precioBase += 50;
-		case 'E':
+			break;
+		case "E":
 			precioBase += 30;
-		case 'F':
+			break;
+		case "F":
 			precioBase += 10;
 		}
 		if (peso >= 0 && peso <= 19) { // Ojo, ninguno de estas condiciones comprueba que el peso es negativo.
@@ -182,18 +208,38 @@ public class Electrodomestico implements Comparable<Electrodomestico> {
 			precioBase += 100;
 		}
 	}
-
+	
 	/**
-	 * Compare to del electrodomestico
+	 * Comprobar color comprueba si el usuario introdujo algún valor dentro del enumerado, si no pone por defecto blanco.
+	 * @param color
+	 * @return
 	 */
-	@Override
-	public int compareTo(Electrodomestico el) {
-		return this.codigo.compareTo(el.codigo);
+	private void comprobarColor(String color) {
+		if (!color.equals("blanco") || !color.equals("negro") || !color.equals("rojo") || !color.equals("azul")
+				|| !color.equals("gris")) {
+			this.color=Color.blanco;
+		} else {
+			this.color=Color.valueOf(color);
+		}
+	}
+	
+	private void comprobarConsumo(char consumo) {
+		if (consumo!='A'|| consumo!='B' || consumo!='C' || consumo!='D' || consumo!='E' ||consumo!='F') {
+			this.consumacion= consumacion.F;
+		}
+		else {
+			this.consumacion=Consumo.valueOf(String.valueOf(consumo));
+		}
 	}
 
+	/**
+	 * ToString del electrodomestico
+	 */
 	@Override
 	public String toString() {
 		return "Código: " + codigo + "\n" + " precio base: " + precioBase + "\n" + "color: " + color + "\n"
-				+ "consumo: " + consumo + "\n" + "peso: " + peso;
+				+ "consumo: " + consumacion + "\n" + "peso: " + peso;
 	}
+
+
 }

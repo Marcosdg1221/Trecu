f5c001b9ae7f6c03cd1fa2bb7fafc257515f6003
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2023-06-15 14:39:28 +0200

    Update Main.java

--- RecuperacionProg/src/ejercicios2/Main.java
+++ RecuperacionProg/src/ejercicios2/Main.java
@@ -1,5 +1,44 @@
 package ejercicios2;
 
-public class Main {
+import java.util.Scanner;
+import java.util.LinkedHashSet;
 
+public class Main {
+	
+	public static LinkedHashSet<Electrodomestico> lista = new LinkedHashSet<Electrodomestico>(); //Puede ser estático
+	public static Scanner all = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int opcion;
+		do {
+			System.out.println("1.Añadir electrodoméstico." + "\n" + "2.Listar electrodomésticos." + "\n"
+					+ "3.Modificar electrodoméstico" + "\n" + "4.Eliminar electrodoméstico" + "\n"
+					+ "5.Guardar en fichero" + "\n" + "0.Salir");
+			opcion = all.nextInt();
+			all.nextLine();
+			switch (opcion) {
+			case 1:
+				Funciones.añadirElectro();		
+				break;
+			case 2:
+				Funciones.listarElectros();
+				break;
+			case 3:
+				Funciones.modificarElectros();
+				break;
+			case 4:
+				Funciones.eliminarElectrodomestico();
+				break;
+			case 5:
+				Funciones.guardarfichero();
+				break;
+			case 0:
+				System.out.println("Salida");
+				break;
+			default:
+				System.out.println("Opción no válida");
+			}
+		} while (opcion != 0);
+	}
 }
